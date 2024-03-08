809ea509c9e6cbcc7f1c9985c20a83c81ad3f8c9
胡政 <huzheng1013@163.com>
2024-03-08 15:13:05 +0800

    feat: 添加request，制定提交规范

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 100, // 每行代码长度（以字符为单位）
+  tabWidth: 2, // 每个缩进级别的空格数
+  semi: false, // 是否在语句末尾添加分号
+  singleQuote: true, // 是否使用单引号而不是双引号
+  trailingComma: none, // 结尾是否添加逗号
+  bracketSpacing: true, // 对象大括号内的首尾空格
+  singleAttributePerLine: false, // 组件或者标签的属性是否控制一行只显示一个属性
+}
---
+++ src/request/request.ts
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import { ElMessage } from 'element-plus'
+
+const request = axios.create({
+  baseURL: 'http://localhost:3000',
+  timeout: 5000,
+})
+
+request.interceptors.request.use(
+  config => {
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+
+request.interceptors.response.use(
+  response => {
+    return response.data
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
